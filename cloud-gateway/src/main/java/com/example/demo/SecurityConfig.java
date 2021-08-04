1b64fba184eb2a2837ddd344919377c05b91bf7b
Partha Bhattacharya <partha@techsophy.com>
2021-08-04 09:27:28 +0530

    Refactored code

--- cloud-gateway/src/main/java/com/example/demo/SecurityConfig.java
+++ cloud-gateway/src/main/java/com/example/demo/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManagerResolver;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.server.ServerWebExchange;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -12,10 +16,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/actuator/health").permitAll()
+                        .anyExchange().authenticated())
                 .oauth2Login(withDefaults());
+        http.oauth2ResourceServer(oauth2->oauth2.jwt());
+
         http.csrf().disable();
         return http.build();
     }
